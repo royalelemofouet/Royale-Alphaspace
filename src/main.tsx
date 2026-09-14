6f80a8e74d507af59ba4534ce0bb9cd375405f48
royalelemofouet <royalelemofouet@gmail.com>
2026-09-14 02:07:12 +0100

    fix: corriger le chemin base pour GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // <--- Indispensable pour éviter les erreurs 404
+  base: '/Royale-Alphaspace/', // Uniquement le nom du dépôt avec les slashes
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
